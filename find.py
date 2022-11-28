05fccb629ce1e8032f4e5a3181f18d0dd83a020b
NITYA NAND JHA <75851120+Shunux-Stuxnet@users.noreply.github.com>
2022-11-28 22:38:13 +0530

    Update find.py

--- find.py
+++ find.py
@@ -2,8 +2,8 @@ from googlesearch import search
 target = input("Who are you looking for? > ")
 
 print("[Searching your target on pastebin.]")
-for x in search("site:pastebin.com " + target, stop=1000, pause=5):
+for x in search("site:pastebin.com " + target, stop=100, pause=5):
     print(x)
 print("[Searching your target on Google.]")
-for y in search(target, stop=1000, pause=1):
+for y in search(target, stop=100, pause=5):
     print(y)    

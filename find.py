bd94eb793d865bfc5a3cbd7eb5bb5342171c1447
NITYA NAND JHA <75851120+Shunux-Stuxnet@users.noreply.github.com>
2022-12-02 16:09:51 +0530

    Delete find.py

--- find.py
+++
@@ -1,9 +0,0 @@
-from googlesearch import search
-target = input("Who are you looking for? > ")
-
-print("[Searching your target on pastebin.]")
-for x in search("site:pastebin.com " + target, stop=100, pause=5):
-    print(x)
-print("[Searching your target on Google.]")
-for y in search(target, stop=100, pause=5):
-    print(y)    

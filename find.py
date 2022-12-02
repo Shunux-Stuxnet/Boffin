9178720671b7a0aab515d8d2cdf766a15236e871
NITYA NAND JHA <75851120+Shunux-Stuxnet@users.noreply.github.com>
2022-12-02 16:41:28 +0530

    Update find.py

--- find.py
+++ find.py
@@ -1,20 +1,7 @@
 from googlesearch import search
-from rich.console import Console
-from rich.table import Table
-
-table = Table(title="Star Wars Movies")
-
-table.add_column("Released", justify="right", style="cyan", no_wrap=True)
-table.add_column("Title", style="magenta")
-table.add_column("Box Office", justify="right", style="green")
 
 def find():
     quote = input("Who are you looking for? > ")
-    print("[Ищем на doxb]")
-    for i1 in search("site:doxbin.org " + quote, stop=1000, pause=1):
-        #print(i1)
-        table.add_row("Doxbin", print(i1), "jy")
-        print(tabulate([['Alice', 24], ['Bob', 19]], headers=['Doxbin', 'Results'], tablefmt='orgtbl'))
     print("[Searching your target on pastebin.]")
     for i2 in search("site:pastebin.com " + quote, stop=1000, pause=5):
         print(i2)

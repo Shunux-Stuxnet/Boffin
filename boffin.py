06c21f255ec982ca335c3c18350583475960ad0f
NITYA NAND JHA <75851120+Shunux-Stuxnet@users.noreply.github.com>
2022-12-02 14:15:05 +0530

    Updation of main file

--- boffin.py
+++ boffin.py
@@ -1,6 +1,9 @@
 import time
 import sys
+from rich.console import Console
+from rich.table import Table
 from find import find
+from eval import eval
 
 green="\u001b[32m"
 Magenta="\033[95m"
@@ -22,10 +25,24 @@ print(Magenta+"""
 print(cyan+"                                                        By : Team Shunux Space")
 print(yellow+"  Input help to see all the options")
 
+def help():
+    table = Table(title="Tools list")
+    table.add_column("S.No", justify="right", style="cyan", no_wrap=True)
+    table.add_column("Tool", style="magenta")
+    table.add_column("Description", justify="right", style="green")
+    table.add_row("1", Find", "Search anything if that is in Pastebin, it also searches the keyword on google.")
+    table.add_row("2", Eval", "Check email validation and MX record.")
+    console = Console()
+    console.print(table)
+
+
+
 def boffin():
     inp=(input("Option-> "))
     if(inp=='1'):
       find()
+    elif(inp=='2'):
+      eval()
     elif(inp=='exit'):
         exit()
     elif(inp=='help'):

79b653f5602846b46d5a820d54ffb54211c4026c
NITYA NAND JHA <75851120+Shunux-Stuxnet@users.noreply.github.com>
2022-12-03 10:50:51 +0530

    Update find.py

--- find.py
+++ find.py
@@ -1,15 +1,23 @@
 from googlesearch import search
 
+cyan = '\033[36m'
 def find():
-    quote = input("Who are you looking for? > ")
-    print("[Searching your target on pastebin.]")
-    for i1 in search("site:pastebin.com " + quote, stop=1000, pause=5):
+    kword = input("Who are you looking for? > ")
+    print("[Searching your target on Doxbin.]")
+    for i in search("site:doxbin.org " + kword, stop=1000, pause=1):
+        print(i)
+    print("[Searching your target on Pastebin.]")
+    for i1 in search("site:pastebin.com " + kword, stop=1000, pause=5):
         print(i1)
-    print("[Searching your target on Google.]")
-    for i2 in search(quote, stop=1000, pause=1):
-        print(i2)
-
-
+    choice = input(cyan+"""
+If you can't see anything in result that means there is nothing related to your keyword on either pastebin or doxbin. 
+        Do you want to search your target on google? Type y/Y to search and n/N to finish.""")
+    if(choice=="y" or choice == "Y"):
+        print("[Searching your target on Google.]")
+        for i2 in search(kword, stop=1000, pause=1):
+            print(i2)
+    else:
+        print("Bye-Bye")
 
 
 if __name__=="__main__":

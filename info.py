834f00c931585da375fe5267f9b33891824bce17
hanma-kun <100011930+hanma-kun@users.noreply.github.com>
2022-12-04 15:52:07 +0530

    Add files via upload

---
+++ info.py
@@ -0,0 +1,17 @@
+from instagramy import InstagramUser as insta
+
+username = input("Enter user name")
+target = insta(username)
+
+print("Username :" ,target.username)
+print("No. of followers:" ,target.number_of_followers)
+print("No. of people following:" ,target.number_of_followings)
+print("Posts :" ,target.number_of_posts)
+print("Privacy Status :" ,target.is_private)
+print("Verification :" ,target.is_verified)
+
+
+
+
+
+

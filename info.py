102b37492aad1944f73c18273d27c0424f0f461d
Ajinkya Bhanpurkar <100011930+hanma-kun@users.noreply.github.com>
2022-12-04 18:05:31 +0530

    Update info.py

--- info.py
+++ info.py
@@ -9,3 +9,18 @@ print("No. of people following:" ,target.number_of_followings)
 print("Posts :" ,target.number_of_posts)
 print("Privacy Status :" ,target.is_private)
 print("Verification :" ,target.is_verified)
+
+
+
+# import the modules
+import instagram_explore as ie
+import json
+  
+# search user name
+result = ie.user('timesofindia')
+  
+parsed_data= json.dumps(result, indent = 4,
+                        sort_keys = True)
+  
+# displaying the data
+print(parsed_data[15:400])
